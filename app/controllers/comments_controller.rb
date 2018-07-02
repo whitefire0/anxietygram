44fe2ace5bb7c3c15fbf72370efee158263ada80
whitefire0 <rickhallett@live.co.uk>
2018-07-02 16:03:22 +0100

    updated route resources, example images

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,16 @@
+class CommentsController < ApplicationController
+  def new
+    @comment = 
+  end
+
+
+  # a new comment will always be linked to an image, can this be determined by the params sent to #new?
+  # the image id/caption/image can be passed to the new/edit form itself
+  # on submit, pass the new comment params back along with the image id
+    # the comment only needs to be saved with the image id.
+    # on successful saving, redirect back to the image show
+
+  def create
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :images
-  resources :users
+  resources :images, only: [:index, :new, :edit, :show, :destroy]
+  resources :users, only: [:new, :edit, :show, :destroy]
+  resources :comments, only: [:new, :edit, :destroy]
+  resources :hashtags, only: [:new, :show]
 
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'

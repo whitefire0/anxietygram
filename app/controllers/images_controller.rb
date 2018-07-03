71f2c42cc8c146331a8a49e1fa0a2ce776ecf576
whitefire0 <rickhallett@live.co.uk>
2018-07-03 13:35:47 +0100

    edit image implemented

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,4 +1,5 @@
 class ImagesController < ApplicationController
+  before_action :set_image, only: [:show, :edit, :update]
   skip_before_action :login_required, only: [:new, :create]
   
   def index
@@ -20,13 +21,30 @@ class ImagesController < ApplicationController
   end
 
   def show
-    @image = Image.find(params[:id])
     @comments = Comment.where(:image_id => params[:id])
+  end
+
+  def edit
+  end
+
+  def update
+    myparams = safe_user_params
+    DebugHelper.mylog("params", "params in image#update", myparams.inspect)
+
+    if @image.update(safe_user_params)
+      redirect_to image_path, notice: "Image edited successfully"
+    else
+      redirect_to edit_image_path, notice: "Image submission error"
+    end
   end
 
   # for each comment displayed in view, also display the username that maps to the user_id FK
 
   private
+
+  def set_image
+    @image = Image.find(params[:id])
+  end
 
   def safe_user_params
     params.require(:image).permit(:graphic, :graphic_file_name, :caption)

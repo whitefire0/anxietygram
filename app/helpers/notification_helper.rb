b814cd08f1b5a3d008bdc89023088bdf701a23ca
whitefire0 <rickhallett@live.co.uk>
2018-07-18 13:48:15 +0100

    notification helper from_time returns time since creation

--- app/helpers/notification_helper.rb
+++ app/helpers/notification_helper.rb
@@ -2,4 +2,8 @@ module NotificationHelper
   def unread_notifications(user)
     user.notifications.where(read: nil).count
   end
+
+  def from_time(created_at)
+    time_ago_in_words(created_at)
+  end
 end
